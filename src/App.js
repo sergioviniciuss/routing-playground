b3d53e52d7ccb25d522cd289cb1b93b70afd6b00
Sérgio Lucena <sergioviniciuss@gmail.com>
2022-06-11 23:25:36 +0200

    feat(Outlet): Adds Layout component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Users } from './pages/Users';
+import { Layout } from './components/Layout';
 
 export const App = () => {
   return (
-    <>
-      <h1>React Router</h1>
-
-      <Navigation />
-
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="home" element={<Home />} />
         <Route path="users" element={<Users />} />
-      </Routes>
-    </>
-  );
-};
+      </Route>
+    </Routes>
 
-const Navigation = () => {
-  return (
-    <nav
-      style={{
-        borderBottom: 'solid 1px',
-        paddingBottom: '1rem',
-      }}
-    >
-      <Link to="/home">Home</Link>
-      <Link to="/users">Users</Link>
-    </nav>
   );
 };
