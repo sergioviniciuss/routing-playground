f3ddfb6701ef0f288736aa0d0a043844684d2623
Sérgio Lucena <sergioviniciuss@gmail.com>
2022-06-12 00:31:23 +0200

    feat(delete User): Uses useNavigate hook to redirect user after deletion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Users } from './pages/Users';
 import { User } from './pages/Users/User';
 import { Layout } from './components/Layout';
 import { NoMatch } from './pages/NoMatch';
-import { users } from './mocks/users';
+import { users as usersList } from './mocks/users';
 
 export const App = () => {
+  const [users, setUsers] = useState(usersList);
+  const navigate = useNavigate();
+  const handleRemoveUser = (userId) => {
+    setUsers((state) => state.filter((user) => user.id !== userId));
+    navigate('/users');
+  };
+
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="users" element={<Users users={users} />}>
-          <Route path=":userId" element={<User />} />
+          <Route
+            path=":userId"
+            element={<User onRemoveUser={handleRemoveUser} />}
+          />
         </Route>
         <Route path="*" element={<NoMatch />} />
       </Route>
--- src/pages/Users/User/index.js
+++ src/pages/Users/User/index.js
@@ -1,11 +1,15 @@
 import { useParams, Link } from "react-router-dom";
 
-export const User = () => {
+export const User = ({ onRemoveUser }) => {
   const { userId } = useParams();
 
   return (
     <>
       <h2>User: {userId}</h2>
+
+      <button type="button" onClick={() => onRemoveUser(userId)}>
+        Remove
+      </button>
 
       <Link to="/users">Back to Users</Link>
     </>
