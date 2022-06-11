a12aaf9ee7f12f0cd83b1c6412f7036fd0d10769
Sérgio Lucena <sergioviniciuss@gmail.com>
2022-06-11 23:36:30 +0200

    feat(default routes): Adds index and NoMatch routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Users } from './pages/Users';
 import { Layout } from './components/Layout';
+import { NoMatch } from './pages/NoMatch';
 
 export const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="users" element={<Users />} />
+        <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
 
