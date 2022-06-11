7b82b90040bb47b7ecf338a9bbc3f29a8b4c34ed
Sérgio Lucena <sergioviniciuss@gmail.com>
2022-06-12 00:40:43 +0200

    feat(Users): filters the users and set search parameters with useSearchParams hook

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,18 +1,43 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = ({ users }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchTerm = searchParams.get('name') || '';
+
+  const handleSearch = (event) => {
+    const name = event.target.value;
+
+    if (name) {
+      setSearchParams({ name: event.target.value });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
     <>
       <h2>Users</h2>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
 
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>
-              {user.fullName}
-            </Link>
-          </li>
-        ))}
+        {users
+          .filter((user) =>
+            user.fullName
+              .toLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          )
+          .map((user) => (
+            <li key={user.id}>
+              <Link to={`/users/${user.id}`}>
+                {user.fullName}
+              </Link>
+            </li>
+          ))}
         <Outlet />
       </ul>
     </>
